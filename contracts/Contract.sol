8018a23257148eaefaa9d718618f2befca8f2a7f
toothtaste <197402001+toothtaste@users.noreply.github.com>
2025-07-03 15:43:55 +0700

    feat

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -6,13 +6,13 @@ import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ContractName is ERC1155, Ownable {
-    uint256 public constant PRICE = 0.00014 ether;
+    uint256 public constant PRICE = 0.00009 ether;
 
     constructor(address initialOwner) ERC1155("ipfs://[CID]/{id}.json") Ownable(initialOwner) {}
 
-    function mint() public payable {
+    function mint(uint256 id) public payable {
         require(msg.value >= PRICE, "Not enough ETH");
-        _mint(msg.sender, 0, 1, "");
+        _mint(msg.sender, id, 1, "");
     }
 
     function withdraw() public onlyOwner {
